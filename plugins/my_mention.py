b45abd2b3c2a2c5059ac20f076ff75c0705ef2be
Capri <matsunagakazuya@matsunagakazuyanoMacBook-puro.local>
2018-11-16 14:13:07 +0900

    he

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -33,34 +33,11 @@ CHANNEL = config.CHANNEL
 def mention_func(message):
     message.reply('もふもふ！☆') # メンション
     message.react('mofumofu')
-    mofumofu_img_list = [
-        "/Users/matsunagakazuya/Desktop/images/gochiusa_sam.gif",
-        "/Users/matsunagakazuya/Desktop/images/e4bd76ac.gif",
-        "/Users/matsunagakazuya/Desktop/images/hFYkpdW.gif",
-        "/Users/matsunagakazuya/Desktop/images/cocoa_chino_hug.gif",
-        "/Users/matsunagakazuya/Desktop/images/tenor.gif"
-    ]
-    files = {'file': open(random.choice(mofumofu_img_list), 'rb')}
-    param = {
-        'token':TOKEN,
-        'channels':CHANNEL,
-        'filename':"filename",
-        'title': "img"
-    }
-    requests.post(url="https://slack.com/api/files.upload",params=param, files=files)
 
 @respond_to('ブス')
 def mention_func(message):
     message.send('(`0言0́*)<ヴェアアアアアアアアアアアアアア')
     message.react('broken_heart')
-    files = {'file': open("/Users/matsunagakazuya/Desktop/images/cocoa_vuxea.gif", 'rb')}
-    param = {
-        'token':TOKEN,
-        'channels':CHANNEL,
-        'filename':"filename",
-        'title': "img"
-    }
-    requests.post(url="https://slack.com/api/files.upload",params=param, files=files)
 
 @listen_to('ランチ食べたい')
 def listen_func(message):
@@ -69,20 +46,3 @@ def listen_func(message):
     lunch_list = ["一平ソバ","東館食堂","旧スエヒロ食堂","松屋","がんま","高園","モンスン"]
     text = '今日は' + random.choice(lunch_list) + 'に行こう！！☆'
     message.reply(text)
-    lunch_img_list = [
-        "/Users/matsunagakazuya/Desktop/images/tumblr_n4m0wlXGoz1rqacixo1_r1_500.gif",
-        "/Users/matsunagakazuya/Desktop/images/90T3gaq.gif",
-        "/Users/matsunagakazuya/Desktop/images/ypDLRyB.gif",
-        "/Users/matsunagakazuya/Desktop/images/99f7111e.gif",
-        "/Users/matsunagakazuya/Desktop/images/cocoa_skirt.gif",
-        "/Users/matsunagakazuya/Desktop/images/cocoa_wan.gif",
-        "/Users/matsunagakazuya/Desktop/images/cocoa_hirari.gif"
-    ]
-    files = {'file': open(random.choice(lunch_img_list), 'rb')}
-    param = {
-        'token':TOKEN,
-        'channels':CHANNEL,
-        'filename':"filename",
-        'title': "img"
-    }
-    requests.post(url="https://slack.com/api/files.upload",params=param, files=files)
