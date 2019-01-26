dbe5dc0e72a335d1479d03677b2fcdf2c33e2084
Capri <matsunagakazuya@matsunagakazuyanoMacBook-puro.local>
2019-01-26 23:54:31 +0900

    regulary_image_post

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -89,34 +89,3 @@ def listen_func(message):
         'title': "img"
     }
     requests.post(url="https://slack.com/api/files.upload",params=param, files=files)
-
-#以下定期的に画像を投稿する処理
-#def image_post_job():
-#   imagefile_list = ['plugins/images/mofumofu/mofumofu_1.gif',
-#                      'plugins/images/mofumofu/mofumofu_2.gif',
-#                      'plugins/images/mofumofu/mofumofu_3.gif',
-#                      'plugins/images/mofumofu/mofumofu_4.gif',
-#                      'plugins/images/mofumofu/mofumofu_5.gif',
-#                      'plugins/images/lunch/lunch_1.gif',
-#                      'plugins/images/lunch/lunch_2.gif',
-#                      'plugins/images/lunch/lunch_3.gif',
-#                      'plugins/images/lunch/lunch_4.gif',
-#                      'plugins/images/lunch/lunch_5.gif',
-#                      'plugins/images/lunch/lunch_6.gif',
-#                      'plugins/images/lunch/lunch_7.gif'] # 画像ファイルのパスを代入
-#    filepath = os.path.abspath(random.choice(imagefile_list))
-    # 画像の投稿
-#    files = {'file': open(filepath, 'rb')}
-#    param = {
-#        'token':TOKEN,
-#        'channels':CHANNEL,
-#        'title': "img"
-#    }
-#    requests.post(url="https://slack.com/api/files.upload",params=param, files=files)
-
-#AM7:30に画像を投稿
-#schedule.every().day.at("7:30").do(image_post_job)
-
-#while True:
-#   schedule.run_pending()
-#    time.sleep(1)
